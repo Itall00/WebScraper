efc5b036fc3c232639c217f1609c168097a6926c
Pablo Zuñiga <pabzuniga@alumnos.uai.cl>
2022-06-28 16:18:28 -0400

    Se arregla csv

--- Funciones.py
+++ Funciones.py
@@ -1,7 +1,14 @@
+import pandas as pd
+
 from Remedios import medicamentos
 from functools import reduce
 import functools
 
+def Csv(Nombre):
+    df = pd.read_csv(Nombre, encoding = 'latin')
+    df.columns=['Principio Activo', 'Farmacia', 'Descripción', 'Precio en Pesos Chilenos', 'Precio en UF']
+    df= df[df['Precio en Pesos Chilenos'] != 0.0]
+    df.to_csv('Medicamentos.csv', index=False)
 
 def Promedio_Activo(Input_Activo):
 
@@ -11,9 +18,9 @@ def Promedio_Activo(Input_Activo):
     print(f"El precio en UF del activo {Input_Activo} es: ")
     print(reduce(lambda suma, p: suma + p.precioUf, activos_filtrados,0)/len(activos_filtrados),"\n")
 
-
-def Maximo_Precio(Input_Activo):
-    print (reduce(lambda a,b : a if a > b else b,medicamentos))
     
 
-    
+def Minimo_Farmacia(Input_Activo, Input_Farmacia):
+    activos_filtrados = list(filter(lambda x: x.activo == Input_Activo and x.farmacia == Input_Farmacia, medicamentos))
+    Minimo_Farmacia = min(activos_filtrados, key=lambda value: value.precioClp)
+    print("\n", Minimo_Farmacia.activo, Minimo_Farmacia.farmacia, Minimo_Farmacia.descripcion, Minimo_Farmacia.precioClp, Minimo_Farmacia.precioUf)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from DataManager import *
 from Ahumada import *
 import csv
+
+from Farmazon import Farma
 from Remedios import medicamentos
 from Simi import Simi
 from UFprice import getUFprice
@@ -11,17 +13,18 @@ def main():
     remedios = read_txt("principios_activos.txt")
 
     Ahumada(remedios)
-    Simi(remedios)
+    #Simi(remedios)
+    #Farma(remedios)
     getUFprice()
 
     with open('Medicamentos.csv', 'w') as Medicamentos_csv:
         writer = csv.writer(Medicamentos_csv)
         for x in medicamentos:
             writer.writerow([x.activo, x.farmacia, x.descripcion, x.precioClp, x.precioUf])
-
+    Csv('Medicamentos.csv')
     for Principio_Activo in remedios:
         Promedio_Activo(Principio_Activo)
-
+        Minimo_Farmacia(Principio_Activo, "Ahumada")
 
 if __name__ == "__main__":
     main()
